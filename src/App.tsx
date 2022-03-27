4bfb81d2447b02ee8960d426e416c7a1d60640d1
Dmitry Malakhov <dmitrii.malakhov@gmail.com>
2022-03-27 13:08:23 +0300

    signin form

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,16 @@
 import styled from 'styled-components';
+import { Routes, Route } from 'react-router-dom';
+import { SignIn } from './routes/SignIn';
+import { Calendar } from './routes/Calendar';
+import 'antd/dist/antd.css';
 
-const App = styled.div``;
+const AppWrapper = styled.div``;
 
-export default App;
+export const App = () => (
+  <AppWrapper>
+    <Routes>
+      <Route path="/" element={<SignIn />} />
+      <Route path="calendar" element={<Calendar />} />
+    </Routes>
+  </AppWrapper>
+);
