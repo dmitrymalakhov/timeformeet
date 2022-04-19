146f4e8acf6433daf4f0309bd8a62934b21bff4c
Dmitry Malakhov <dmitrii.malakhov@gmail.com>
2022-04-19 23:12:00 +0300

    connect form with db

--- src/routes/SignIn/index.tsx
+++ src/routes/SignIn/index.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Form, Input, Button, Checkbox } from "antd";
+import { Form, Input, Button, Checkbox, Alert } from "antd";
 import { Navigate } from "react-router-dom";
 import styled from "styled-components";
 import { UserOutlined, LockOutlined } from "@ant-design/icons";
@@ -11,82 +11,108 @@ const LoginButtonWrapper = styled(Form.Item)`
   flex-direction: column;
 `;
 
+interface SignInResponce {
+  statusCode: number;
+  error: string;
+  message: string;
+}
+
 export const SignIn = () => {
   const [loggedIn, setLoggedIn] = useState<boolean>(false);
-  console.log(loggedIn);
+  const [error, setError] = useState<string | null>(null);
 
-  const onFinish = (values: any) => {
+  const handleCloseError = () => {
+    setError(null);
+  };
+
+  const handleFinish = (values: any) => {
     fetch("https://rbuobh.sse.codesandbox.io/signin", {
       method: "POST",
       body: JSON.stringify(values)
     })
       .then((res) => res.json())
-      .then((res) => {
-        setLoggedIn(!!res);
-      });
+      .then((res: SignInResponce) => {
+        if (res.statusCode && res.statusCode !== 200) {
+          setError(res.message);
+        } else {
+          setLoggedIn(!!res);
+        }
+      })
+      .catch((e) => console.log(e));
   };
 
   return (
-    <SignInWrapper>
-      <Form
-        name="normal_login"
-        className="login-form"
-        initialValues={{
-          remember: true
-        }}
-        onFinish={onFinish}
-      >
-        <Form.Item
-          name="login"
-          rules={[
-            {
-              required: true,
-              message: "Please input your Login!"
-            }
-          ]}
+    <>
+      <SignInWrapper>
+        <Form
+          name="normal_login"
+          className="login-form"
+          initialValues={{
+            remember: true
+          }}
+          onFinish={handleFinish}
         >
-          <Input
-            prefix={<UserOutlined className="site-form-item-icon" />}
-            placeholder="Login"
-          />
-        </Form.Item>
-        <Form.Item
-          name="password"
-          rules={[
-            {
-              required: true,
-              message: "Please input your Password!"
-            }
-          ]}
-        >
-          <Input
-            prefix={<LockOutlined className="site-form-item-icon" />}
-            type="password"
-            placeholder="Password"
-          />
-        </Form.Item>
-        <Form.Item>
-          <Form.Item name="remember" valuePropName="checked" noStyle>
-            <Checkbox>Remember me</Checkbox>
-          </Form.Item>
-
-          <a className="login-form-forgot" href="">
-            Forgot password
-          </a>
-        </Form.Item>
-        <LoginButtonWrapper>
-          <Button
-            block
-            type="primary"
-            htmlType="submit"
-            className="login-form-button"
+          <Form.Item
+            name="login"
+            rules={[
+              {
+                required: true,
+                message: "Please input your Login!"
+              }
+            ]}
           >
-            Log in
-          </Button>
-          Or <a href="/signup">register now!</a>
-        </LoginButtonWrapper>
-      </Form>
-      {loggedIn && <Navigate replace to="/calendar" />}
-    </SignInWrapper>
+            <Input
+              prefix={<UserOutlined className="site-form-item-icon" />}
+              placeholder="Login"
+            />
+          </Form.Item>
+          <Form.Item
+            name="password"
+            rules={[
+              {
+                required: true,
+                message: "Please input your Password!"
+              }
+            ]}
+          >
+            <Input
+              prefix={<LockOutlined className="site-form-item-icon" />}
+              type="password"
+              placeholder="Password"
+            />
+          </Form.Item>
+          <Form.Item>
+            <Form.Item name="remember" valuePropName="checked" noStyle>
+              <Checkbox>Remember me</Checkbox>
+            </Form.Item>
+
+            <a className="login-form-forgot" href="">
+              Forgot password
+            </a>
+          </Form.Item>
+          <LoginButtonWrapper>
+            <Button
+              block
+              type="primary"
+              htmlType="submit"
+              className="login-form-button"
+            >
+              Log in
+            </Button>
+            Or <a href="/signup">register now!</a>
+          </LoginButtonWrapper>
+          {error && (
+            <Alert
+              description={error}
+              type="error"
+              closable
+              onClose={handleCloseError}
+            />
+          )}
+        </Form>
+
+        {loggedIn && <Navigate replace to="/calendar" />}
+      </SignInWrapper>
+    </>
   );
 };
