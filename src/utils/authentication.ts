ab8a33c5b5b818fbc7bbcd6a4fbf9bff459d6d55
Dmitry Malakhov <dmitrii.malakhov@gmail.com>
2023-08-06 12:30:10 +0000

    Update 6 files

--- src/utils/authentication.ts
+++ src/utils/authentication.ts
@@ -1,22 +1,34 @@
-import { apiRequest } from "./request";
+import { apiRequest } from './request';
 
-interface RefreshTokenResponse {
+export enum HttpStatus {
+  Unauthorized = 401,
+  Forbidden = 403
+}
+
+export interface RefreshTokenResponse {
   accessToken?: string;
   refreshToken?: string;
 }
 
-export const refreshToken = () =>
-  apiRequest("/refresh")
+export const refreshToken = (): Promise<RefreshTokenResponse> =>
+  apiRequest('/refresh')
     .then((res: Response) => {
-      if ([401, 403].includes(res.status)) {
-        window.location.replace("/signin");
+      if (
+        [HttpStatus.Unauthorized, HttpStatus.Forbidden].includes(res.status)
+      ) {
+        window.location.replace('/signin');
       }
 
       return res.json();
     })
     .then((res: RefreshTokenResponse) => {
-      if (res && res.accessToken)
-        localStorage.setItem("token", res.accessToken);
+      if (res.accessToken) {
+        localStorage.setItem('token', res.accessToken);
+      }
 
       return res;
+    })
+    .catch((error: Error) => {
+      console.error('An error occurred while refreshing the token:', error);
+      throw error; // Re-throw the error if you want to handle it further up the chain
     });
